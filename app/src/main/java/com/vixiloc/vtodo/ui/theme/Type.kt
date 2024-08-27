5f06b919c30f369e7f7985ee5fffce7c010469c8
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-08-27 19:47:13 +0700

    modify font family

--- app/src/main/java/com/vixiloc/vtodo/ui/theme/Type.kt
+++ app/src/main/java/com/vixiloc/vtodo/ui/theme/Type.kt
@@ -2,33 +2,119 @@ package com.vixiloc.vtodo.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.vixiloc.vtodo.R
 
 // Set of Material typography styles to start with
+val defaultTypography = Typography()
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.bodyLarge.fontWeight,
+        fontSize = defaultTypography.bodyLarge.fontSize,
+        lineHeight = defaultTypography.bodyLarge.lineHeight,
+        letterSpacing = defaultTypography.bodyLarge.letterSpacing
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.bodySmall.fontWeight,
+        fontSize = defaultTypography.bodySmall.fontSize,
+        lineHeight = defaultTypography.bodySmall.lineHeight,
+        letterSpacing = defaultTypography.bodySmall.letterSpacing
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.bodyMedium.fontWeight,
+        fontSize = defaultTypography.bodyMedium.fontSize,
+        lineHeight = defaultTypography.bodyMedium.lineHeight,
+        letterSpacing = defaultTypography.bodyMedium.letterSpacing
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.titleSmall.fontWeight,
+        fontSize = defaultTypography.titleSmall.fontSize,
+        lineHeight = defaultTypography.titleSmall.lineHeight,
+        letterSpacing = defaultTypography.titleSmall.letterSpacing
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.titleMedium.fontWeight,
+        fontSize = defaultTypography.titleMedium.fontSize,
+        lineHeight = defaultTypography.titleMedium.lineHeight,
+        letterSpacing = defaultTypography.titleMedium.letterSpacing
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.titleLarge.fontWeight,
+        fontSize = defaultTypography.titleLarge.fontSize,
+        lineHeight = defaultTypography.titleLarge.lineHeight,
+        letterSpacing = defaultTypography.titleLarge.letterSpacing
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.headlineSmall.fontWeight,
+        fontSize = defaultTypography.headlineSmall.fontSize,
+        lineHeight = defaultTypography.headlineSmall.lineHeight,
+        letterSpacing = defaultTypography.headlineSmall.letterSpacing
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.headlineMedium.fontWeight,
+        fontSize = defaultTypography.headlineMedium.fontSize,
+        lineHeight = defaultTypography.headlineMedium.lineHeight,
+        letterSpacing = defaultTypography.headlineMedium.letterSpacing
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.headlineLarge.fontWeight,
+        fontSize = defaultTypography.headlineLarge.fontSize,
+        lineHeight = defaultTypography.headlineLarge.lineHeight,
+        letterSpacing = defaultTypography.headlineLarge.letterSpacing
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.displaySmall.fontWeight,
+        fontSize = defaultTypography.displaySmall.fontSize,
+        lineHeight = defaultTypography.displaySmall.lineHeight,
+        letterSpacing = defaultTypography.displaySmall.letterSpacing
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.displayMedium.fontWeight,
+        fontSize = defaultTypography.displayMedium.fontSize,
+        lineHeight = defaultTypography.displayMedium.lineHeight,
+        letterSpacing = defaultTypography.displayMedium.letterSpacing
+    ),
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.displayLarge.fontWeight,
+        fontSize = defaultTypography.displayLarge.fontSize,
+        lineHeight = defaultTypography.displayLarge.lineHeight,
+        letterSpacing = defaultTypography.displayLarge.letterSpacing
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.labelSmall.fontWeight,
+        fontSize = defaultTypography.labelSmall.fontSize,
+        lineHeight = defaultTypography.labelSmall.lineHeight,
+        letterSpacing = defaultTypography.labelSmall.letterSpacing
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.labelMedium.fontWeight,
+        fontSize = defaultTypography.labelMedium.fontSize,
+        lineHeight = defaultTypography.labelMedium.lineHeight,
+        letterSpacing = defaultTypography.labelMedium.letterSpacing
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.poppins)),
+        fontWeight = defaultTypography.labelLarge.fontWeight,
+        fontSize = defaultTypography.labelLarge.fontSize,
+        lineHeight = defaultTypography.labelLarge.lineHeight,
+        letterSpacing = defaultTypography.labelLarge.letterSpacing
     )
-    */
 )
