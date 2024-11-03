1573fa7229eae4f8dda9f14e5d61cea741e0b504
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-11-03 16:17:52 +0700

    update: theme = modify statusBar color

--- app/src/main/java/com/vixiloc/vtodo/ui/theme/Theme.kt
+++ app/src/main/java/com/vixiloc/vtodo/ui/theme/Theme.kt
@@ -1,10 +1,16 @@
 package com.vixiloc.vtodo.ui.theme
 
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import com.materialkolor.rememberDynamicColorScheme
 
 @Composable
@@ -13,6 +19,19 @@ fun VTodoTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = rememberDynamicColorScheme(Color(0xFF0094FF), darkTheme)
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        /* getting the current window by tapping into the Activity */
+        val currentWindow = (view.context as? Activity)?.window
+            ?: throw Exception("Not in an activity - unable to get Window reference")
+
+        SideEffect {
+            currentWindow.statusBarColor = Color.Black.copy(alpha = 0.3f).toArgb()
+            WindowCompat.getInsetsController(currentWindow, view).isAppearanceLightStatusBars =
+                darkTheme
+        }
+    }
+
 
     MaterialTheme(
         colorScheme = colorScheme.copy(surface = Color(0xFFF4F4F4)),
