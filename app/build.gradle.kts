eb767d797855e8b39b3a7a9f5043569bdd1a6051
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-11-03 14:33:28 +0700

    update: library = upgrade AGP version from 8.5.0 to 8.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.vixiloc.vtodo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.vixiloc.vtodo"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
